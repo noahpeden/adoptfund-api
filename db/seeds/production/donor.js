5f3df3cddc14dc61ecc65b950c9f5d6ca850b5ad
Noah Peden <noahpeden@gmail.com>
2017-04-16 13:24:14 -0600

    fixed production seed data

--- db/seeds/production/donor.js
+++ db/seeds/production/donor.js
@@ -3,14 +3,12 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return Promise.all([
       knex('donation').insert({
-        userId: 1,
         familyId: 1,
         donationAmount: '$1',
         created_at: new Date,
         updated_at: new Date,
       }),
       knex('donation').insert({
-        userId: 2,
         familyId: 2,
         donationAmount: '$2',
         created_at: new Date,
